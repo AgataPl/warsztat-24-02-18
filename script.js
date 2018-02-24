d4cf3e0028f96740a8638e6b81d27510d3be8ef3
Agata Płecha <agata16200@gmail.com>
2018-02-24 12:50:37 +0100

    object

--- script.js
+++ script.js
@@ -31,3 +31,30 @@ var simplestObj = {
     lastname: "Plecha"
 }
 console.log(simplestObj)
+
+//nested objects with data
+
+var nastedObj = {
+    name: "Agata",
+    car: {
+        brand: "VW",
+        model: 4
+    }
+}
+console.log(nastedObj)
+console.log(nastedObj.car.model)
+console.log(nastedObj.car.year)
+
+//arrays
+var simpleArray = [
+    1,
+    {name: 'Ala', array: [1,4,6]},
+    3,
+    4,
+    5
+]
+console.log(simpleArray)
+console.log(simpleArray[4])
+console.log(simpleArray[1].name)
+console.log(simpleArray[1]['name'])
+
