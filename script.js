bed16433a93789f38c8ba9602874bc90032e3727
Agata Płecha <agata16200@gmail.com>
2018-02-24 12:28:32 +0100

    primitives

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log('Hello Agata!')
+
+// data types in JS
+
+// strig
+var textVariable = "Agata"
+console.log(textVariable)
+
+//number
+var numberVariable = 43
+console.log(numberVariable)
+
+//boolean
+var boolVariable = true
+console.log(boolVariable)
+
+// null
+var nulVariable = null
+console.log(nulVariable)
+
+// undefined
+var undefinedVariable = undefined
+console.log(undefinedVariable)
