a240c853ff57ea4fb88789b98e2d035ba622daff
Agata Płecha <agata16200@gmail.com>
2018-02-24 16:36:23 +0100

    game

--- script.js
+++ script.js
@@ -1,10 +1,81 @@
-var body = document.querySelector('body')
+(function(){
+    var points
+    var time
+    var mole
+    var gameIntervalId
 
-console.log(body)
-
-body.addEventListener(
-    'click',
-    function(){
-        console.log('Klik!')
+    function addPoint(){
+        points++
+        displayPoints(points)
     }
-)
+
+    function reduceTime(){
+        time--
+        displayTime(time)
+    }
+
+    function displayPoints(pointsParam){
+        var pointsContainer = document.querySelector('.points')
+        pointsContainer.innerText = pointsParam
+    }
+
+    function displayTime(timeParam){
+        var timeContainer = document.querySelector('.time')
+        timeContainer.innerText = timeParam
+    }
+
+
+// tworzenie kreta
+    function makeMole(){
+
+        
+        var molePosX = Math.round(
+            Math.random() * (window.innerWidth - window.innerHeight / 10)
+        )
+        var molePosY = Math.round(
+            Math.random() * (window.innerHeight - window.innerHeight / 10)
+        )
+
+        var mole = document.createElement('div')
+
+        mole.classList.add('mole')
+
+        mole.style.left = molePosX + 'px'
+        mole.style.top = molePosY + 'px'
+
+
+        //kliknięcie kreta
+        mole.addEventListener(
+            'click',
+            function(){
+                mole.remove()
+                addPoint()
+            }
+        )
+
+        document.querySelector('body').appendChild(mole)
+
+        return mole
+    }
+
+    function init(){
+        points = 0
+        time = 10
+        mole = makeMole()
+
+        displayPoints(points)
+        displayTime(time)
+
+        gameIntervalId = setInterval(
+            function(){
+                mole.remove()
+                mole = makeMole()
+                reduceTime()
+            },
+            1000
+        )
+    }
+
+    init()
+
+})()
