f42adb0444a6009e4c6fa1f96fa29ab8b9beb5a4
Agata Płecha <agata16200@gmail.com>
2018-02-24 13:40:07 +0100

    interval + global and local

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-// function declaration
-function hello(name, numberOfLogs){
-    for(var i = 0; i<numberOfLogs; i++){
-        console.log("Hello " + name + "!")
+var seconsCounter = 1
 
-    }
+setInterval(
+    function (){
+        console.log(seconsCounter++)
+
+    },
+    1000
+)
+
+var globalVriable = 'GLOBAL'
+
+function add(a, b){
+    var someVariable = 123
+    globalVriable = 321
+    console.log(globalVriable)
+    return a + b
 }
 
-//function invocation (call)
-hello('Mateusz', 1)
-hello('Agata', 2)
-hello('Jan', 3)
-hello({name: 'Janina'},1)
+//function expression
+var add2=function (a, b){
+    var someVariable = 123
+    globalVriable = 321
+    console.log(globalVriable)
+    return a + b
+}
+
+//assiging function to another variable
+var add3 = add2
+var result = add(3,5)
+console.log(result)
+console.log(someVariable)
+
 
