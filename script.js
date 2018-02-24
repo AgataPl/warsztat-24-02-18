482923016254a1da58358611d4d077983a862e96
Agata Płecha <agata16200@gmail.com>
2018-02-24 13:00:00 +0100

    first function

--- script.js
+++ script.js
@@ -1,60 +1,11 @@
-console.log('Hello Agata!')
-
-// data types in JS
-
-//primitives
-
-// strig
-var textVariable = "Agata"
-console.log(textVariable)
-
-//number
-var numberVariable = 43
-console.log(numberVariable)
-
-//boolean
-var boolVariable = true
-console.log(boolVariable)
-
-// null
-var nulVariable = null
-console.log(nulVariable)
-
-// undefined
-var undefinedVariable = undefined
-console.log(undefinedVariable)
-
-//objects
-
-var simplestObj = {
-    name: "Agata",
-    lastname: "Plecha"
+// function declaration
+function hello(){
+    console.log("Hello!")
+    console.log("Hello!")
+    console.log("Hello!")
 }
-console.log(simplestObj)
 
-//nested objects with data
-
-var nastedObj = {
-    name: "Agata",
-    car: {
-        brand: "VW",
-        model: 4
-    }
-}
-console.log(nastedObj)
-console.log(nastedObj.car.model)
-console.log(nastedObj.car.year)
-
-//arrays
-var simpleArray = [
-    1,
-    {name: 'Ala', array: [1,4,6]},
-    3,
-    4,
-    5
-]
-console.log(simpleArray)
-console.log(simpleArray[4])
-console.log(simpleArray[1].name)
-console.log(simpleArray[1]['name'])
-
+//function invocation (call)
+hello()
+hello()
+hello()
