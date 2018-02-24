1beb37a3526d08ae15064e6cc446855661c3488b
Agata Płecha <agata16200@gmail.com>
2018-02-24 13:42:51 +0100

    intervals and timeouts

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var seconsCounter = 1
 
-setInterval(
+var intervalId = setInterval(
     function (){
         console.log(seconsCounter++)
 
@@ -8,27 +8,9 @@ setInterval(
     1000
 )
 
-var globalVriable = 'GLOBAL'
-
-function add(a, b){
-    var someVariable = 123
-    globalVriable = 321
-    console.log(globalVriable)
-    return a + b
-}
-
-//function expression
-var add2=function (a, b){
-    var someVariable = 123
-    globalVriable = 321
-    console.log(globalVriable)
-    return a + b
-}
-
-//assiging function to another variable
-var add3 = add2
-var result = add(3,5)
-console.log(result)
-console.log(someVariable)
-
-
+setTimeout(
+    function(){
+        clearInterval(intervalId)
+    },
+    5000
+)
